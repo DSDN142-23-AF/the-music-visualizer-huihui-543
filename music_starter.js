fa2d8cfa1834b5250906b167e891d09ba6281179
huihui-543 <151787971+huihui-543@users.noreply.github.com>
2024-02-08 10:03:01 +0700

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,18 +1,18 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  colorMode(HSB, 100);
-  background(255); 
+  colorMode(HSB, 200);
+  background(300); 
   rectMode(CENTER); 
   strokeWeight(9);
-  stroke(drum, 80, 80); // Use the 'drum' variable for the hue
+  stroke(bass, 80, 80); // Use the 'drum' variable for the hue
   
-  var drumMap = map(drum, 0, 100, 30, 90); // Map 'drum' to a new range
-  var LengthOfLine = 300;
+  var drumMap = map(bass, 0, 100, 30, 90); // Map 'drum' to a new range
+  var LengthOfLine = 100;
   var LineStart = 100;
   var lineEnd = LineStart + LengthOfLine; // Fixed variable name
   
-  for (var i = 1; i < 5; i++) {
+  for (var i = 1; i <= 5; i++) {
     var LineStep = i * 10; // Corrected to 'i * 10'
     line(LineStart, LineStep, lineEnd, LineStep); // Fixed 'lineStep' to 'LineStep'
   }
