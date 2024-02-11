78af8d26f886549a4f36b00bc76cfba0add92d7b
huihui-543 <151787971+huihui-543@users.noreply.github.com>
2024-02-11 22:14:13 +0700

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,21 +1,18 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  colorMode(HSB, 200);
-  background(300); 
-  rectMode(CENTER); 
-  strokeWeight(9);
-  stroke(bass, 80, 80); // Use the 'drum' variable for the hue
-  
+  let stripeWidth = map(other, 0, 90, 0, 7, true);
 
-}
-let triangleHeight = map(bass, 40, 100, 200, 550, true);
-fill(117,200,174); // teal
-for(let i=0; i<3; i++) {
-  let cur_x = map(i, 0, 4, 0, width);
-  let next_x = map(i+1, 0, 3, 0, width);
-  let mid_x = (cur_x + next_x) / 2.0;
-  //let cur_y = 4 * height / 5;
-  let cur_y = height 
-  triangle(cur_x, cur_y, mid_x, cur_y - triangleHeight, next_x, cur_y);
+  fill('blue'); // 
+  
+  for (let i = 0; i < height; i += stripeWidth * 2) { // multiply by 2 to alternate stripes
+    beginShape();
+    for (let x = 0; x <= width; x += 20) { // increment x by 20 to make it smooth
+      let y = i + sin(x) * 10; //  wave height
+      vertex(x, y);
+    }
+    vertex(width, i + stripeWidth);
+    vertex(0, i + stripeWidth);
+    endShape(CLOSE);
+  }
 }
