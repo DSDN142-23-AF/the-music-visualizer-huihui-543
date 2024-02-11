225bbb8cdb3b596e56af0e1493340a60eeb9288f
huihui-543 <151787971+huihui-543@users.noreply.github.com>
2024-02-11 23:20:28 +0700

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,18 +1,38 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  let stripeWidth = map(other, 0, 90, 0, 10, true);
-
-  fill(30,100,300); // 
   
-  for (let i = 0; i < height; i += stripeWidth * 2) { // multiply by 2 to alternate stripes
-    beginShape();
-    for (let x = 0; x <= width; x += 20) { // increment x by 20 to make it smooth
-      let y = i + sin(x) * 10; //  wave height
-      vertex(x, y);
-    }
-    vertex(width, i + stripeWidth);
-    vertex(0, i + stripeWidth);
-    endShape(CLOSE);
+  let waveHeight = map(vocal, 40, 100, 200, 550, true);
+  fill(400, 50, 40); // red
+  for(let i = 0; i < width; i += 10) {
+    let y = height / 3+ sin(i * 1+ millis() * 2) * waveHeight;
+    ellipse(i, y, 10, 10);
   }
+
+let ovalPlace = map(other, 20, 100, height-50, 30, true);
+let ovalSize = map(other, 10, 20, 60, 250, true);
+fill(255, 255, 0); // yellow 
+ellipse(width/2.1, ovalPlace, ovalSize);
+
+stroke(255, 105, 0); // orange
+line(width/2 - ovalSize, ovalPlace, width/2 - ovalSize - 100, ovalPlace); // left
+line(width/2 + ovalSize, ovalPlace, width/2 + ovalSize + 100, ovalPlace); // rioght
+line(width/2, ovalPlace - ovalSize, width/2, ovalPlace - ovalSize - 50); // top
+line(width/2, ovalPlace + ovalSize, width/2, ovalPlace + ovalSize + 50); // bottom
+
+let stripeWidth = map(vocal, 0, 100, 100, 20, true);
+
+fill(0,0,0); // 
+
+for (let i = 0; i < height; i += stripeWidth * 2) { // multiply by 2 to alternate stripes
+  beginShape();
+  for (let x = 0; x <= width; x += 20) { // increment x by 20 to make it smooth
+    let y = i + sin(x) * 10; //  wave height
+    vertex(x, y);
+  }
+  vertex(width, i + stripeWidth);
+  vertex(0, i + stripeWidth);
+  endShape(CLOSE);
 }
+}
+
