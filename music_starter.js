6857431cfb5167a185711bc984704d06642588f6
huihui-543 <151787971+huihui-543@users.noreply.github.com>
2024-02-12 03:11:39 +0700

    photos

--- music_starter.js
+++ music_starter.js
@@ -1,30 +1,52 @@
-
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
-function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  
-  let waveHeight = map(vocal, 40, 100, 200, 550, true);
-  fill(250, 80, 40); // red
-  for(let i = 0; i < width; i += 10) {
-    let y = height / 2.5+ sin(i * 1+ millis() * 50) * waveHeight;
-    ellipse(i, y, 10, 10);
+
+let firstRun = true
+
+  Let pic = []
+function draw_one_frame(vocal, drum, bass, other) {
+if (firstRun) {
+rectMode (CENTER);
+pic.push(loadImage('pic1.png'));
+pic.push(loadImage('pic2.png'));
+pic.push(loadImage('pic3.png'));
+pic.push(loadImage('pic4.png'));
+firstRun = false
+
+}
+
+var VocalFrame =int(map(vocal, 0,100, 0,2));
+console. log (VocalFrame);
+push ();
+scale (0.5);
+image(pic|VocalFramel, width/2, height/2)
+pop ();
+
+}
+
+
+
+
+  let dotHeight = map(vocal, 40, 100, 200, 550, true);
+  fill(250, 55, 40); // red orange
+  for(let i = 0; i < width; i += 10) {    
+    let y = height / 2.5+ sin(i * 1+ millis() * 50) * dotHeight; // learnt from a tuakana
+    ellipse(i, y, 10, 20);
   }
 
-let ovalPlace = map(other, 20, 100, height-50, 30, true);
+  
+let ovalPlace = map(other, 10, 100, height-50, 30, true);
 let ovalSize = map(other, 10, 20, 60, 250, true);
 fill(255, 255, 0); // yellow 
-ellipse(width/2.1, ovalPlace, ovalSize);
+ellipse(width/2.05, ovalPlace, ovalSize);
 
-stroke(400, 50, 0); // orange
-line(width/2 - ovalSize, ovalPlace, width/2 - ovalSize - 100, ovalPlace); // left
-line(width/2 + ovalSize, ovalPlace, width/2 + ovalSize + 100, ovalPlace); // rioght
-line(width/2, ovalPlace - ovalSize, width/2, ovalPlace - ovalSize - 50); // top
-line(width/2, ovalPlace + ovalSize, width/2, ovalPlace + ovalSize + 50); // bottom
+stroke(400, 15, 0); // red
+
 
 let stripeWidth = map(vocal, 0, 100, 100, 20, true);
 
 fill(0,0,0); // 
 
-for (let i = 0; i < height; i += stripeWidth * 2) { // multiply by 2 to alternate stripes
+for (let i = 0; i < height; i += stripeWidth * 2) { // multiply by 2 to alternate stripes, learnt from a tuakana
   beginShape();
   for (let x = 0; x <= width; x += 20) { // increment x by 20 to make it smooth
     let y = i + sin(x) * 10; //  wave height
@@ -34,5 +56,5 @@ for (let i = 0; i < height; i += stripeWidth * 2) { // multiply by 2 to alternat
   vertex(0, i + stripeWidth);
   endShape(CLOSE);
 }
-}
+
 
